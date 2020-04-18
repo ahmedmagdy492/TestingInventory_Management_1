f295d53aecbf0281896855d1b059cc8b1aaf8cc5
ahmedmagdy <ahmedmagdy492@yahoo.com>
2020-04-18 16:29:22 +0200

    Setup Core and Repository

---
+++ Inventory_Management/Inventory_Management/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management
+{
+    public interface IProduct
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        DateTime ProductionDate { get; set; }
+        DateTime ExpiryDate { get; set; }
+    }
+}
---
+++ Inventory_Management/Inventory_Management/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management
+{
+    public class Product : IProduct
+    {
+        public int Id { get ; set ; }
+        public string Name { get ; set ; }
+        public DateTime ProductionDate { get ; set ; }
+        public DateTime ExpiryDate { get ; set ; }        
+    }
+}
